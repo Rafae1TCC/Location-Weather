281400d8afdcc6f3468f510a0722913209011214
Rafael Cabrera <rafael.cabrera32@uabc.edu.mx>
2026-08-27 17:42:51 -0700

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Rafael Cabrera on 8/27/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
